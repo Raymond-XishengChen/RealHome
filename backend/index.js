763553af2cc96fa5703638689e442369ba605b8b
RaymondChen <xi.chen2210@gmail.com>
2023-10-28 09:41:19 +1100

    Created api call and model for listings

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // Use middleware to handle error messages
 app.use((err, req, res, next) => {
