c8c4ddd8462564b4c6174678501382865f584cc8
RaymondChen <xi.chen2210@gmail.com>
2023-10-30 05:20:37 +1100

    deploying V2

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 // Use dotenv to hide password
 // Create connections to database and log status of success or failure
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/realhome').then( () => {
+mongoose.connect(process.env.MONGODB_URI || 3000).then( () => {
     console.log("Connected to Mongo!");
 }).catch((err) => {
     console.log(err);
