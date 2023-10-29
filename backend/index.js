bfdef3cd10fd99621a37cdb4780ba1fdbbfa6e17
RaymondChen <xi.chen2210@gmail.com>
2023-10-30 05:43:01 +1100

    deploying V4

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 app.listen(port, host, () => {
-    console.log("Server started...");
+    console.log(`Server started, listening to ${ port }`);
 });
 
 app.use("/api/user", userRouter);
