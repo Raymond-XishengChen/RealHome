9f627e38288ea80206205f49ff29ea9ae244e1b9
RaymondChen <xi.chen2210@gmail.com>
2023-10-30 19:22:43 +1100

    deploy test

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 // Use dotenv to hide password
@@ -14,6 +15,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/realhome'
 }).catch((err) => {
     console.log(err);
 })
+
+// Create a dynamic directory path
+const __dirname = path.resolve();
 
 // Create the app for express, and allow server to pass JSON messages
 const app = express();
@@ -31,6 +35,14 @@ app.listen(port, host, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+// Create a static folder by joining the path name
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// Any request not following the api user, auth and listing, will run inside the client->dist folder
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+})
 
 // Use middleware to handle error messages
 app.use((err, req, res, next) => {
