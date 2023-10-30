81e7ce71af5d2b540c78a2174a45bc82ab66e5df
RaymondChen <xi.chen2210@gmail.com>
2023-10-30 21:03:21 +1100

    deploy changes v3

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   // Adding a proxy so that everytime when a request occurs to the "api" route, add the localhost 3000 to the beginning
   server:{
+    port: 3001,
+     open:true,
     proxy: {
         "/api": {
         target: "http://localhost:3000",
         secure: false,
+        changeOrigin: true,
       },
     },
   },
